a61dd3399c42ca3e0f97125544f2c9c1f2f91d1f
Jane <evgeniya-l@bk.ru>
2020-07-05 17:52:11 +0300

    add Variable.java

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte coresNum = 2; 
+		short memory = 8000;
+		long frequency = 2_500_000_000l;
+		int typeSystem = 64;
+		float availableMemory = 7.79f;
+		double frequency2 = 2.5;
+		char processor = 'i';
+		boolean isWorking = true;
+
+		System.out.println(coresNum);
+		System.out.println(memory);
+		System.out.println(frequency);
+		System.out.println(typeSystem);
+		System.out.println(availableMemory);
+		System.out.println(frequency2);
+		System.out.println(processor);
+		System.out.println(isWorking);
+	}
+	
+}
