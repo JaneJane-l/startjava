474bf9e5d30c842c021cb33364e4d09974c0cf58
Jane <evgeniya-l@bk.ru>
2020-07-08 19:28:40 +0300

    change Cycle.java

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -5,5 +5,20 @@ public class Cycle {
 			result=i;
 			System.out.println(result);
 		}
+		int j = 6;
+		while (j >= -6){
+			System.out.println(j);
+			j = j - 2;
+		}
+		int k = 10;
+		int result_2 = 0;
+		do {
+			if (k % 2 != 0){
+				result_2 = result_2+k;
+			}
+			k++;
+
+		} while (k <= 20);
+		System.out.println(result_2);
 	}
 }
